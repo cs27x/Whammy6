2d2aadba467f0a2c7f6217b3a25e815f073fa7c7
edixius <ethan.b.dixius@vanderbilt.edu>
2014-10-14 12:12:08 -0500

    Added test file for methods 5-8

--- src/main/java/org/magnum/cs278/testdriven/App.java
+++ src/main/java/org/magnum/cs278/testdriven/App.java
@@ -83,7 +83,6 @@ public class App {
 		return evtsAtLocation;
 	}
 
-
 	public List<Event> getTodaysEvents() throws Exception {
 		List<Event> todaysEvents = new ArrayList<Event>();
 		List<Event> events = getParkSpecialPermits();
@@ -146,7 +145,6 @@ public class App {
 		}
 		return temp;
 	}
-	
 
 	public List<Event> getEventsLargerThan(int i)  throws Exception {
 		// TODO Auto-generated method stub
