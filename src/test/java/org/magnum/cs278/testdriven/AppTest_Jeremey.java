ba336ea82067dcf3341454901daa6f3952c2fefe
jbkey <jeremey.b.key@vanderbilt.edu>
2014-10-14 12:10:33 -0500

    Commit Jeremey's Tests

---
+++ src/test/java/org/magnum/cs278/testdriven/AppTest_Jeremey.java
@@ -0,0 +1,77 @@
+package org.magnum.cs278.testdriven;
+
+
+import java.util.List;
+
+import org.magnum.cs278.testdriven.App;
+import org.magnum.cs278.testdriven.Event;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest_Jeremey {
+
+	private App app = new App();
+	
+	@Test
+	public void TestGetEventsInJune() throws Exception 
+	{
+		List<Event> juneEvents = app.getEventsInJune();
+		assertNotNull(juneEvents);
+		
+		for(Event curEvent : juneEvents)
+		{
+			assertNotNull(curEvent);
+			assertNotNull(curEvent.getMonth());
+			
+			assertEquals(curEvent.getMonth(),"Jun");
+		}
+	}
+	
+	@Test
+    public void TestGetRiverfrontParkSpecialPermits() throws Exception 
+	{
+		List<Event> riverEvents = app.getRiverfrontParkSpecialPermits();
+		assertNotNull(riverEvents);
+		
+		for(Event curEvent : riverEvents)
+		{
+			assertNotNull(curEvent);
+			assertNotNull(curEvent.getLocation());
+			
+			assertEquals(curEvent.getLocation(),"Riverfront Park");
+		}
+    }
+	
+	@Test
+	public void TestAttendanceGreaterThanFive() throws Exception 
+	{
+		List<Event> events = app.AttendanceGreaterThanFive();
+		assertNotNull(events);
+		
+		for(Event curEvent : events)
+		{
+			assertNotNull(curEvent);
+			assertNotNull(curEvent.getAttendance());
+			
+			assertTrue(Integer.parseInt(curEvent.getAttendance()) > 5);
+		}
+
+	}
+
+	@Test
+	public void TestGetParkSpecialPermitsByAttendance() throws Exception 
+	{
+		List<Event> events = app.getParkSpecialPermitsByAttendance();
+		assertNotNull(events);
+		
+		for(Event curEvent : events)
+		{
+			assertNotNull(curEvent);
+		}
+	}
+}
